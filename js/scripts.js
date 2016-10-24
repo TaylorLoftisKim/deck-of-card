9aead49bddce1c1632ac08a76c49759f544f7711
Cody Brubaker and Taylor Loftis-Kim <taylor@>
2016-10-24 16:46:25 -0700

    add javascript for deck of cards

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+var suits = ["Spades", "Clubs", "Diamonds", "Hearts"];
+var numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+
+$(document).ready(function() {
