67d4b35e698c8282d21c270e43fba6835dc0d5e1
loren glenn and taylor loftis-kim <taylor@>
2016-10-25 09:51:17 -0700

    make javascript function useing for loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-
+var result = [];
 var suits = ["Spades", "Clubs", "Diamonds", "Hearts"];
 var numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 
-$(document).ready(function() {
+var calculate = function() {
+  for (var index = 0; index < suits.length; index++) {
+    numbers.forEach(function(number) {
+      result.push(number + " of " + suits[index]);
+    });
+  };
+  alert(result);
+};
+
+$(document).ready(function(event) {
+  $('button#submit').click(calculate());
+  event.preventDefault();
+});
